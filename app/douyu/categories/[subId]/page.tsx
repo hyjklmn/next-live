490b8d8203ceee25c5886dc967d594fe5f005f9c
zhilei <858899665@qq.com>
2023-09-06 21:31:27 +0800

    fix: params

--- app/douyu/categories/[subId]/page.tsx
+++ app/douyu/categories/[subId]/page.tsx
@@ -1,18 +1,19 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'next/navigation'
 import { getCategoryRooms, searchAnchors } from '@/lib/apis/douyu'
 import RoomCard from '@/components/RoomCard'
 import { LiveResult } from '@/lib/types/apis'
-export default function CategoryRoomPage() {
-  const params = useParams()
-  const subId = params.subId as string
+
+type Params = {
+  subId: string
+}
+export default function CategoryRoomPage({ params }: { params: Params }) {
   const [roomData, setRoomData] = useState<LiveResult>({
     hasMore: true,
     roomItems: []
   })
   async function categoryRooms() {
-    const data = await getCategoryRooms(subId)
+    const data = await getCategoryRooms(params.subId)
     setRoomData(data)
   }
   useEffect(() => {
