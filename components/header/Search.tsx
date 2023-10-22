68b514ae65ff774c5360b38c7c1e6e58efd750ff
zhilei <858899665@qq.com>
2023-10-22 23:54:00 +0800

    feat:serch init

--- components/header/Search.tsx
+++ components/header/Search.tsx
@@ -1,12 +1,57 @@
+import { useRef } from "react"
+import { useRouter } from 'next/navigation'
+
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-// import React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { NavLinks } from "@/lib/NavLinks"
+
 export default function Search() {
+  const platFormVal = useRef("")
+  const typeVal = useRef("")
+  const searchVal = useRef("")
+  const router = useRouter()
+  function searchBlur(event: React.FocusEvent<HTMLInputElement>) {
+    searchVal.current = event.target.value
+  }
+
+  function searchClick() {
+    if (searchVal.current === '' && platFormVal.current === '' && typeVal.current === '') return
+    router.push(`/search?keyword=${searchVal.current}&type=${typeVal.current}&platform=${platFormVal.current}`)
+  }
   return (
     <div className="flex items-center space-x-2">
-      <Input type="text" placeholder="Email" />
-      <Button size="icon" variant="outline" className="rounded-full shrink-0">
+      <Select onValueChange={(value) => { platFormVal.current = value }}>
+        <SelectTrigger>
+          <SelectValue placeholder="平台" />
+        </SelectTrigger>
+        <SelectContent>
+          {
+            NavLinks.map((link) => (
+              <SelectItem key={link.key} value={link.key.toString()}>{link.title}</SelectItem>)
+            )
+          }
+
+        </SelectContent>
+      </Select>
+      <Select onValueChange={(value) => { typeVal.current = value }}>
+        <SelectTrigger>
+          <SelectValue placeholder="类型" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="房间号">房间号</SelectItem>
+          <SelectItem value="主播">主播</SelectItem>
+        </SelectContent>
+      </Select>
+      <Input type="text" placeholder="Search..." className="w-[200px]" onBlur={searchBlur} />
+      <Button size="icon" variant="outline" className="rounded-full shrink-0" onClick={searchClick}>
         <MagnifyingGlassIcon />
       </Button>
     </div>
