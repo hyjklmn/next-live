6dce6670fbde17bc07029f003c8c57442ccd3547
zhilei <858899665@qq.com>
2023-10-30 23:19:04 +0800

    feat:anchorcard status

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -78,26 +78,35 @@ function AnchorCard(props: any) {
   const imageLoader = ({ src, width }: ImageLoaderProps) => {
     return `${src}?w=${width}`
   }
-
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4'>
+    <div className='relative grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4'>
       {
         props.list.map((anchor: any) => {
           return (
-            <div key={anchor.roomId} className="flex flex-col items-center border p-8 transition-colors duration-200 transform cursor-pointer group hover:bg-blue-600 rounded-xl">
+            <div key={anchor.roomId} className="flex flex-col items-center border px-2 py-6 transition-colors duration-200 transform cursor-pointer group hover:bg-blue-600 rounded-xl">
               <Image className="object-cover w-28 h-28 rounded-full ring-4 ring-gray-300"
                 loader={imageLoader} src={anchor.avatar}
                 priority
                 width={0}
                 height={0}
-                alt="" />
+                alt={anchor.userName} />
               <p className="mt-4 text-base font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white">{anchor.userName}</p>
               <p className="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">房间号:{anchor.roomId}</p>
-              {anchor.liveStatus ? '直播中' : '未直播'}
+              {
+                anchor.liveStatus ? <LiveStatusBadge text='直播中' /> : ''
+              }
             </div>
           )
         })
       }
     </div>
+  )
+}
+
+function LiveStatusBadge({ text }: { text: string }) {
+  return (
+    <span className={`absolute right-3 top-3 inline-block p-1 text-xs rounded bg-red-500`}>
+      {text}
+    </span>
   )
 }
